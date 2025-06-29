cbc1dc7b575010f234ae5b5f9ea7d2a1aa88edf8
Maria Vasconcelos <mariavasconcelos2201@gmail.com>
2025-06-29 18:58:52 -0300

    Atualizações

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 const swiperProjects = new Swiper(".swiper-projects .swiper", {
     loop:true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 40,
     navigation: {
         nextEl: '.swiper-projects-next',
@@ -8,5 +8,40 @@ const swiperProjects = new Swiper(".swiper-projects .swiper", {
     },
     autoplay: {
         delay: 2000,
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 3
+        },
+        572: {
+            slidesPerView: 2
+        }
+
     }
+});
+
+const buttonMenu = document.getElementById("menu-open");
+const menu = document.getElementById("header-mobile");
+const closeButtonMenu = document.getElementById("closeMenu");
+
+buttonMenu.addEventListener("click", function() {
+    
+    // Acessibilidade
+    menu.setAttribute("aria-hidden", "false");
+    buttonMenu.setAttribute("aria-expanded", "true");
+    closeButtonMenu.setAttribute("aria-expanded", "true");
+
+    menu.classList.add("active");
+
+});
+
+closeButtonMenu.addEventListener("click", function() {
+
+    // Acessibilidade
+    menu.setAttribute("aria-hidden", "true");
+    buttonMenu.setAttribute("aria-expanded", "false");
+    closeButtonMenu.setAttribute("aria-expanded", "false");
+    
+    menu.classList.remove("active");
+
 });
